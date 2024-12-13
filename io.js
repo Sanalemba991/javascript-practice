b3d376f0162a412ca74ff7506baba61b37d6e9ea
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-13 17:15:21 +0530

    if else used it

---
+++ io.js
@@ -0,0 +1,10 @@
+let sam = 9000;
+let ram = 11110;
+const op = sam >= ram;  // This is correct, it will evaluate to true because 9000 >= 100
+
+if(op){
+    console.log("The condition is true");
+}
+else{
+    console.log("The condition is false");
+}
