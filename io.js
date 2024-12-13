e7c309eb5e45d894a44baddc91a864cfd46132ab
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-13 17:20:49 +0530

    and shijinnaba

--- io.js
+++ io.js
@@ -1,10 +1,20 @@
-let sam = 9000;
-let ram = 11110;
-const op = sam >= ram;  // This is correct, it will evaluate to true because 9000 >= 100
+// let sam = 9000;
+// let ram = 11110;
+// const op = sam >= ram;  // This is correct, it will evaluate to true because 9000 >= 100
 
-if(op){
-    console.log("The condition is true");
-}
-else{
-    console.log("The condition is false");
-}
+// if(op){
+//     console.log("The condition is true");
+// }
+// else{
+//     console.log("The condition is false");
+// }
+
+//&& shijinnnaba gi
+let hour=111;
+
+if(hour>=90 && hour<=100)
+ {
+    console.log("VERY GOod")
+ }else{
+    console.log("Re enter")
+ }
