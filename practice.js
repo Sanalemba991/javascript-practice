b44aea24ec73ebfad397c49bc17a0d5fafbe7445
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-12-17 23:33:57 +0530

    pravtice

--- practice.js
+++ practice.js
@@ -1,10 +1,11 @@
 //check vowel exist or not
-const ram = "Sanalemba";
-const vowels = "sana"; 
+// const ram = "Sanalemba";
+// const vowels = "sana"; 
 
-for (let i = 0; i < ram.length; i++) {
-    if (!vowels.includes(ram[i])) { 
-        console.log(ram[i]);
-    }
-}
+// for (let i = 0; i < ram.length; i++) {
+//     if (!vowels.includes(ram[i])) { 
+//         console.log(ram[i]);
+//     }
+// }
 
+const ram=document.getElementById("sam").innerHTML=("Samba")
